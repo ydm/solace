fac5b0d572b3d3e69ce9351b739ae6e72b08d560
Yordan Miladinov <yordanm@proton.me>
2024-08-31 13:21:27 +0300

    initial

--- script/AliasRegistry.s.sol
+++ script/AliasRegistry.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {AliasRegistry} from "../src/AliasRegistry.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract AliasRegistryScript is Script {
+    AliasRegistry public registry;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        registry = new AliasRegistry();
 
         vm.stopBroadcast();
     }
---
+++ test/AliasRegistry.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {AliasRegistry} from "../src/AliasRegistry.sol";
+
+contract AliasRegistryTest is Test {
+    AliasRegistry public reg;
+
+    function setUp() public {
+        reg = new AliasRegistry();
+        reg.set(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, "a");
+    }
+
+    function test() public {
+        assertEq(reg.get(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266), "a");
+
+        assertEq(reg.get(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), "");
+        reg.set(0x70997970C51812dc3A010C7d01b50e0d17dc79C8, "b");
+        assertEq(reg.get(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), "b");
+    }
+}
